f13ef8450057c1c0dd4548046d0bdfef9543a368
João Gabriel Salvador Paiva <joaogabrielsalvadorpaiva@gmail.com>
2023-08-30 22:14:19 -0300

    Implementação do SQLAlchemy como ORM.

--- database.py
+++ database.py
@@ -1,84 +1,121 @@
-import sqlite3
+from sqlalchemy import Column, Integer, String, BLOB
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import json
-from typing import Union, Optional
+from typing import Union, Optional, Tuple
 
-def searchBookByTitle(id:int): # id: int -> json (title, author, description), file (pdf : bytes)
-    cnx = sqlite3.connect("banco.sqlite")
-    cursor = cnx.cursor()
+#config
+Engine = create_engine("sqlite:///banco.db")
+Base = declarative_base()
+Session = sessionmaker(bind=Engine)
 
-    query = f"SELECT * FROM books WHERE title = {id}"
-    cursor.execute(query)
-    book_data = cursor.fetchone()
+class Books(Base):
+    __tablename__ = "books"
 
-    cnx.close()
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    author = Column(String(255), nullable=False)
+    about = Column(String(255), nullable=False)
+    book = Column(BLOB, nullable=False)
+
+    def __repr__(self):
+        return f"""
+            'id': {self.id},\n
+            'title': {self.title},\n
+            'author': {self.author},\n
+            'about': {self.about},\n
+            'book': {str(self.book)}
+        """ 
+    def __str__(self):
+        return self.__repr__(self)
+
+def allBooks():
+    currentSession = Session()
+    try:
+        book_data = currentSession.query(Books).all()
+        return book_data
+    except Exception as e:
+        raise Exception(e)
+
+def searchBookByID(id:int): # id: int -> json (title, author, about), file (pdf : bytes)
+    currentSession = Session()
+    book_data = currentSession.query(Books).filter_by(id=id).first()
 
     if book_data:   
         book = {
-            'id': book_data[0],
-            'title': book_data[1],
-            'author': book_data[2],
-            'description': book_data[3],
+            'id': book_data.id,
+            'title': book_data.title,
+            'author': book_data.author,
+            'about': book_data.about
         }
-        return (json.dumps(book, indent=4), book_data[4])
+        return (json.dumps(book, indent=4), book_data.book)
     else:
         return 404
 
-def searchBookByTitle(title:str): # title: str -> json (title, author, description), file (pdf : bytes)
-    cnx = sqlite3.connect("banco.sqlite")
-    cursor = cnx.cursor()
-
-    query = f"SELECT * FROM books WHERE title = {title}"
-    cursor.execute(query)
-    book_data = cursor.fetchone()
-
-    cnx.close()
+def searchBookByTitle(title:str): # title: str -> json (title, author, about), file (pdf : bytes)
+    currentSession = Session()
+    book_data = currentSession.query(Books).filter_by(title=title).first()
 
     if book_data:   
         book = {
-            'id': book_data[0],
-            'title': book_data[1],
-            'author': book_data[2],
-            'description': book_data[3],
+            'id': book_data.id,
+            'title': book_data.title,
+            'author': book_data.author,
+            'about': book_data.about
         }
-        return (json.dumps(book, indent=4), book_data[4])
+        return (json.dumps(book, indent=4), book_data.book)
     else:
         return 404
 
 #NOT IMPLEMENTED (For front-end choice hub)
 def titles():
-    cnx = sqlite3.connect("banco.sqlite")
-    cursor = cnx.cursor()
-
-    query = f"SELECT name from books;"
-    cursor.execute(query)
-    book_data = cursor.fetchone()
-
-    cnx.close()
-
-    if book_data:   
-        book = {
-            'id': book_data[0],
-            'title': book_data[1],
-            'author': book_data[2],
-            'description': book_data[3],
-            'first_chapter': book_data[4]
-        }
-        return json.dumps(book, indent=4)
+    currentSession = Session()
+    data = [i.title for i in currentSession.query(Books).all()]
+    if data:
+        return data
     else:
-        return json.dumps({'error': 'Book not found'}, indent=4)
+        return 404
 
-async def addBook(title, author, description: Optional[str], book: Union[str, bytes]):
-    cnx = sqlite3.connect("banco.sqlite")
-    cursor = cnx.cursor()
+async def addBook(title, author, about: Optional[str], book: Union[str, bytes]):
 
-    query = f"INSERT INTO books VALUES(?, ?, ?, ?)"
-    cursor.execute(query, (title, author, description, book))
-    book_data = cursor.fetchone()
+    currentSession = Session()
 
-    cnx.close()
+    if isinstance(book, str):
+        book = bytes(book, encoding="utf-8")
 
+    data_insert = Books(title=title, author=author, about=about, book=book)
+    try:
+        currentSession.add(data_insert)
+        currentSession.commit()
+        return 201
+    except Exception as e:
+        currentSession.rollback()
+        raise Exception(e, "RollBack executed.")
+
+def delBookById(id:int):
+    currentSession = Session()
+    try:
+        currentSession.query(Books).filter_by(id=id).delete()
+    except Exception as e:
+        currentSession.rollback()
+        raise Exception(e, "RollBack executed")
+
+def delBookByIdRange(start: int, end: int):
+    for _ in range(start, end+1):
+        delBookById(start)
+
+def delBookByTitle(title:str):
+    currentSession = Session()
+    try:
+        currentSession.query(Books).filter_by(title=title).delete()
+    except Exception as e:
+        currentSession.rollback()
+        raise Exception(e, "RollBack executed")
 
 if __name__ == "__main__":
   #PARA TESTES
-  book_json = searchBookByID(1)
-  print(book_json)
+
+  print(searchBookByID(1))
+  print(searchBookByTitle("Harry Potter and the Sorcerers Stone"))
+  
